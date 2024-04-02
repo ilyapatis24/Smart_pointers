fcdd94897d6eccc78837fa8b1119a128bdf4e0c5
ilyapatis24 <ilyapatis24@mail.ru>
2024-04-02 21:34:04 +0300

    initial commit

---
+++ 01/main.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+
+class tridiagonal_matrix
+{
+public:
+    std::vector<double> m_down;
+    std::vector<double> m_upper;
+    std::vector<double> m_middle;
+    tridiagonal_matrix(
+        const std::vector<double>& down,
+        const std::vector<double>& upper,
+        const std::vector<double>& middle) :
+        m_down{ down }, m_upper{ upper }, m_middle{ middle }
+    {};
+    ~tridiagonal_matrix() { std::cout << "destructor called\n"; }
+
+    std::unique_ptr<tridiagonal_matrix> clone() {
+        auto clone{ std::make_unique<tridiagonal_matrix>(m_down, m_upper, m_middle) };
+        std::cout << "\nfunction clone was called\n";
+
+        return clone;
+    }
+};
+
+
+int main(int argc, char** argv) {
+    setlocale(LC_ALL, "Russian");
+
+    auto down = std::vector<double>{ -4.0, 5.0 };
+    auto upper = std::vector<double>{ 14.0, 8.0 };
+    auto middle = std::vector<double>{ 3.0, 1.0, 7.0 };
+    auto matrix = std::make_unique<tridiagonal_matrix>(
+        down,
+        upper,
+        middle
+    );
+    std::cout << matrix.get() << "\n";
+    auto matrix_clone = matrix->clone();
+    std::cout << std::endl;
+    std::cout << matrix_clone << std::endl;
+    return 0;
+}
