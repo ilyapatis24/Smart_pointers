dbd3324401fc76ecfd8ca07933dda88737473690
ilyapatis24 <ilyapatis24@mail.ru>
2024-04-02 22:59:27 +0300

    third mission is completed

---
+++ 03/UniquePtr.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include <iostream>
+#include <utility>
+
+
+template<class T>
+class UniquePtr
+{
+private:
+	T* _ptr = nullptr;
+
+public:
+	UniquePtr() { std::cout << "Constructor called  " << '\n'; };
+	UniquePtr(T* ptr) : _ptr(ptr) { std::cout << this << '\n'; };
+	UniquePtr(const UniquePtr&) = delete; // запрещаем конструктор копирования
+	UniquePtr& operator=(const UniquePtr&) = delete; // запрещаем оператор присваивания
+	T& operator*() const
+	{
+		return *_ptr;
+	}
+
+	T* operator->() const
+	{
+		return _ptr;
+	}
+
+	T* release() noexcept
+	{
+		T* _TempPtr = _ptr;
+		_ptr = nullptr;
+		return _TempPtr;
+	}
+
+	UniquePtr(UniquePtr&& other) : _ptr(other._ptr)
+	{
+		std::cout << "UniquePtr&& " << this << '\n';
+		other._ptr = nullptr;
+	}
+
+	~UniquePtr()
+	{
+		std::cout << "Destructor called" << '\n';
+		delete _ptr;
+	};
+};
+
---
+++ 03/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <memory>
+
+#include "UniquePtr.h"
+
+int main(int argc, char** argv) {
+    setlocale(LC_ALL, "Russian");
+    auto ptr = UniquePtr<int>(new int(10));
+    std::cout << "ptr = " << *ptr << std::endl;
+    std::cout << "ptr = " << ptr.release() << '\n';
+    return 0;
+}
